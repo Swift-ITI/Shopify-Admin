777f833c06bac983de81f03c046348ff407750be
Adham Samer <48602871+AdhamSamer3301@users.noreply.github.com>
2023-03-02 08:11:51 +0200

    Login Added

---
+++ Shopify-Admin/Views/AuthenticationScreen/LoginVC.swift
@@ -0,0 +1,44 @@
+//
+//  LoginVC.swift
+//  Shopify-Admin
+//
+//  Created by Adham Samer on 02/03/2023.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+    @IBOutlet var pwTxtField: UITextField! {
+        didSet {
+            pwTxtField.layer.cornerRadius = 10
+            pwTxtField.layer.borderColor = UIColor(named: "SecondaryColor")?.cgColor
+            pwTxtField.layer.borderWidth = 2
+        }
+    }
+
+    @IBOutlet var loginImage: UIImageView! {
+        didSet {
+            loginImage.image = UIImage(named: "Splash")
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func loginBtn(_ sender: Any) {
+        if pwTxtField.text == "1234" {
+            performSegue(withIdentifier: "goToTabBar", sender: self)
+        } else {
+            showAlert()
+        }
+    }
+
+    func showAlert() {
+        let alert = UIAlertController(title: "Wrong Credentials!", message: "Enter new password", preferredStyle: UIAlertController.Style.alert)
+
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+
+        present(alert, animated: true, completion: nil)
+    }
+}
