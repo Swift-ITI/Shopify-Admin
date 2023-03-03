bab63ffe607c6371ef61f56336088e982f08ac54
ZeinabMohamed <zm4816035@gmail.com>
2023-03-03 09:06:10 -0800

    ProductDetailsUi

--- Shopify-Admin/Views/HomeScreen/HomeVC.swift
+++ Shopify-Admin/Views/HomeScreen/HomeVC.swift
@@ -59,7 +59,7 @@ extension HomeVC: UICollectionViewDelegate, UICollectionViewDataSource, UICollec
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         let invDetail = self.storyboard?.instantiateViewController(withIdentifier: "inventoryDetails") as! ProductDetailsVC
-        invDetail.product = products?[indexPath.row]
+      //  invDetail.product = products?[indexPath.row]
         self.navigationController?.pushViewController(invDetail, animated: true)
     }
 }
--- Shopify-Admin/Views/HomeScreen/ProductDetailsVC.swift
+++ Shopify-Admin/Views/HomeScreen/ProductDetailsVC.swift
@@ -8,32 +8,31 @@
 import UIKit
 
 class ProductDetailsVC: UIViewController {
-    @IBOutlet var sku: UITextField! {
-        didSet {
-            sku.layer.borderWidth = 2
-            sku.layer.borderColor = UIColor(named: "SecondaryColor")?.cgColor
-            sku.layer.cornerRadius = 10
-        }
-    }
-
-    @IBOutlet var productName: UILabel!
-    @IBOutlet var quantityTxtField: UITextField!
-    @IBOutlet var available: UILabel!
-    @IBOutlet var steepper: UIStepper!
-    @IBOutlet var quantatiy: UILabel!
-    var product: Product?
-    
+//    @IBOutlet var sku: UITextField! {
+//        didSet {
+//            sku.layer.borderWidth = 2
+//            sku.layer.borderColor = UIColor(named: "SecondaryColor")?.cgColor
+//            sku.layer.cornerRadius = 10
+//        }
+//    }
+//
+//    @IBOutlet var quantityTxtField: UITextField!
+//    @IBOutlet var available: UILabel!
+//    @IBOutlet var steepper: UIStepper!
+//    @IBOutlet var quantatiy: UILabel!
+//    var product: Product?
+//
     override func viewDidLoad() {
         super.viewDidLoad()
-        productName.text = product?.title
-        sku.text = product?.variants?.first?.sku
-        steepper.value = Double(product?.variants?.first?.inventory_quantity ?? 0)
-        available.text = product?.variants?.first?.inventory_quantity.formatted()
-        quantatiy.text = String(Int(steepper.value))
+       // productName.text = product?.title
+       // sku.text = product?.variants?.first?.sku
+       // steepper.value = Double(product?.variants?.first?.inventory_quantity ?? 0)
+     //   available.text = product?.variants?.first?.inventory_quantity.formatted()
+    //    quantatiy.text = String(Int(steepper.value))
         // Do any additional setup after loading the view.
     }
 
-    @IBAction func stepper(_ sender: Any) {
-        quantatiy.text = String(Int(steepper.value))
-    }
+//    @IBAction func stepper(_ sender: Any) {
+//        quantatiy.text = String(Int(steepper.value))
+//    }
 }
