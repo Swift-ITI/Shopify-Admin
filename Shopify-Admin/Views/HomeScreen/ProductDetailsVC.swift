c8713814b7dae8488cb465097b0f1d78a45c4520
Adham Samer <48602871+AdhamSamer3301@users.noreply.github.com>
2023-03-03 16:19:17 +0200

    Product Details UI Done

--- Shopify-Admin/Views/HomeScreen/ProductDetailsVC.swift
+++ Shopify-Admin/Views/HomeScreen/ProductDetailsVC.swift
@@ -7,23 +7,22 @@
 
 import UIKit
 
-class InventoryDetails: UIViewController {
-
-    
-    @IBOutlet weak var sku: UITextField! {
+class ProductDetailsVC: UIViewController {
+    @IBOutlet var sku: UITextField! {
         didSet {
             sku.layer.borderWidth = 2
             sku.layer.borderColor = UIColor(named: "SecondaryColor")?.cgColor
             sku.layer.cornerRadius = 10
         }
     }
-    @IBOutlet weak var productName: UILabel!
+
+    @IBOutlet var productName: UILabel!
+    @IBOutlet var quantityTxtField: UITextField!
+    @IBOutlet var available: UILabel!
+    @IBOutlet var steepper: UIStepper!
+    @IBOutlet var quantatiy: UILabel!
+    var product: Product?
     
-    @IBOutlet weak var quantityTxtField: UITextField!
-    @IBOutlet weak var available: UILabel!
-    @IBOutlet weak var steepper: UIStepper!
-    @IBOutlet weak var quantatiy: UILabel!
-    var product:Product?
     override func viewDidLoad() {
         super.viewDidLoad()
         productName.text = product?.title
@@ -33,7 +32,7 @@ class InventoryDetails: UIViewController {
         quantatiy.text = String(Int(steepper.value))
         // Do any additional setup after loading the view.
     }
-    
+
     @IBAction func stepper(_ sender: Any) {
         quantatiy.text = String(Int(steepper.value))
     }
